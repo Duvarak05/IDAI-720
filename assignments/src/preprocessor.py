823e729fbf576e7da85ea2639e8b64b8e3fb3ec8
Duvarakanath <92026285+Duvarak05@users.noreply.github.com>
2026-03-26 14:47:22 -0400

    Refactor Reweighing function to simplify A handling

--- assignments/src/preprocessor.py
+++ assignments/src/preprocessor.py
@@ -3,21 +3,14 @@ import numpy as np
 
 def Reweighing(X, Y, A):
     Y = np.array(Y)
-
-    # If A is column name(s), extract from X
-    try:
-        A_vals = list(X[A].itertuples(index=False, name=None))
-    except:
-        A_vals = np.array(A)
-        if A_vals.ndim == 1:
-            A_vals = [(a,) for a in A_vals]
+    A = np.array(A)
 
     n = len(Y)
 
     # counts
     P_y = Counter(Y)
-    P_a = Counter(A_vals)
-    P_ay = Counter(zip(A_vals, Y))
+    P_a = Counter(A)
+    P_ay = Counter(zip(A, Y))
 
     # convert to probabilities
     for k in P_y:
@@ -27,12 +20,12 @@ def Reweighing(X, Y, A):
     for k in P_ay:
         P_ay[k] /= n
 
+    # compute weights
     sample_weight = np.zeros(n)
-
     for i in range(n):
-        sample_weight[i] = (P_y[Y[i]] * P_a[A_vals[i]]) / P_ay[(A_vals[i], Y[i])]
+        sample_weight[i] = (P_y[Y[i]] * P_a[A[i]]) / P_ay[(A[i], Y[i])]
 
-    # rescale
+    # Rescale the sum of sample weights to len(y)
     sample_weight = sample_weight * len(Y) / sum(sample_weight)
 
     return sample_weight
